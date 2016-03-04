e9436da258973cf612fb1fdf497d08ddbb9d311a
saavedrajl <jlds161089@gmail.com>
2016-03-04 11:09:58 -0500

    Quitamos object-assign, ya que con ES6, podemos extender la clase ... eso daria practicamente lo mismo

---
+++ app/stores/index.jsx
@@ -0,0 +1,13 @@
+import Dispatcher import '../dispatcher'
+import constants import '../constants'
+
+// sirve para fucionar objetos, 
+// es decir generar un obj3 con las propiedades de obj1 y obj2
+import assign from 'object-assign'
+
+// sirve para generar eventos
+// on: crea un evento
+// emit: dispara un evento creado
+import {EventEmitter} from 'events'
+
+
