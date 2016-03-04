bf5b08cbcfcf36a990be36b639076def1e22ae15
saavedrajl <jlds161089@gmail.com>
2016-03-04 14:03:26 -0500

    Documentamos algunas cosas de Store y Action

--- app/actions/index.jsx
+++ app/actions/index.jsx
@@ -3,13 +3,19 @@
 import Dispatcher import '../dispatcher'
 import constants import '../constants'
 
+// metodos que notifican a traves del "dispatcher",
+// una accion a uno o mas "stores"
 let actions = {
+	// a travez del "dispatcher" indicamos al "store"
+	// que vamos a agregar un dato a las lista de "tasks"
 	addTask: function(task) {
 		Dispatcher.handleAction({
 			actionType: constants.ADD_TASK,
 			data: task
 		})
 	},
+	// a travez del "dispatcher" indicamos al "store"
+	// que vamos a remover un dato de la lista de "tasks"
 	removeTask: function(id) {
 		Dispatcher.handleAction({
 			actionType: constants.REMOVE_TASK,
--- app/stores/index.jsx
+++ app/stores/index.jsx
@@ -8,7 +8,10 @@ import constants import '../constants'
 // emit: dispara un evento creado
 import {EventEmitter} from 'events'
 
-
+// Nombre del evento que sera creado desde el "component"
+// solo con llamar la funcion "addChangeListener" de este "store".
+// Despues de creado, podran ser accedidas desde los metodos
+// del "Action"
 const CHANGE_EVENT = 'change'
 
 // variable que contendra los datos de la aplicacion
@@ -16,14 +19,16 @@ let _store = {
 	list: []
 }
 
-// La clase 'Store' hereda la clase 'EventEmitter'
+// La clase "Store" hereda la clase "EventEmitter"
+// esta clase nos permite definir un evento para el store
+// las funciones deberan ser ejecutadas desde el "component"
 class Store extends EventEmitter {
 	addChangeListener(handle) {
 		this.on(CHANGE_EVENT, handle)
 	}
 
 	removeChangeListener(handle) {
-		this.on(CHANGE_EVENT, handle)
+		this.removeListener(CHANGE_EVENT, handle)
 	}
 
 	getList() {
@@ -32,9 +37,10 @@ class Store extends EventEmitter {
 }
 
 // instanciamos el store
-
 const store = new Store()
 
+// suscribimos(registramos) las acciones que seran
+// accedidas desde los actions (para publicar algun dato)
 Dispatcher.register((payload) => {
 	const action = payload.action
 
